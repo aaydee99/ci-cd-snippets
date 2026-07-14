1aaef85dabe6a16a49330f72bf14a9d283b92ca2
Abdullah Akhtar <44116978+aaydee99@users.noreply.github.com>
2026-07-14 15:26:11 +0500

    Delete snippets/my_api_snippet.js

--- snippets/my_api_snippet.js
+++
@@ -1,8 +0,0 @@
-export default {
-    async fetch(request) {
-      const response = await fetch(request);
-      const newResponse = new Response(response.body, response);
-      newResponse.headers.set("X-Deployed-By", "GitHub-Actions");
-      return newResponse;
-    },
-};
